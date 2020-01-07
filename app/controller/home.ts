bc1596d8bd86ea1cfcff68597db2751744e9f13a
xiaoxiang.shen <xiaoxaing.shen@zhangmen.com>
2020-01-07 19:14:18 +0800

    test

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,7 +2,7 @@ import { Controller } from 'egg';
 
 export default class HomeController extends Controller {
   public async index() {
-    const { ctx } = this;
+    const { ctx } = this
     ctx.body = await ctx.service.test.sayHi('egg'); 
   }
 }
