2b489fc6fb8b6d563c7cdbe3729ae5c0a36f9699
sxxRaul <975547545@qq.com>
2020-01-09 15:36:54 +0800

    test

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -2,8 +2,7 @@ import { Controller } from 'egg';
 
 export default class HomeController extends Controller {
   public async index() {
-    const { ctx } = this; 
-    console.log(56) 
+    const { ctx } = this
     
     
     ctx.body = await ctx.service.test.sayHi('egg')
