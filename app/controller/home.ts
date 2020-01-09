b3f719623fc490f0f480318811656887768178d1
xiaoxiang.shen <xiaoxaing.shen@zhangmen.com>
2020-01-09 15:23:40 +0800

    test

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -4,9 +4,8 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this; 
     console.log(56) 
-    console.log(456)
-    console.log(568)
-    console.log(789) 
+    
+    
     ctx.body = await ctx.service.test.sayHi('egg')
   }
 }
